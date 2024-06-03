89545ae2098a7a3c1e108c9ef50b0ea164b9ba07
Dawid-Femin <d.femin@wp.pl>
2024-06-03 23:33:33 +0200

    Created navigation component

--- client/src/components/Navigation/Navigation.tsx
+++ client/src/components/Navigation/Navigation.tsx
@@ -1,12 +1,23 @@
+import classes from "./navigation.module.scss";
+import useMobile from "../../hooks/useMobile.tsx";
+import { MdMenu } from "react-icons/md";
+import { InputSearch } from "../InputSearch/InputSearch.tsx";
+
 export const Navigation = () => {
+  const isMobile = useMobile();
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-          <a href="/contact">Contact</a>
-        </li>
-      </ul>
-    </nav>
+    <div className={classes.root}>
+      <div className={classes.continer}>
+        <div className={classes.logo}>Personal shop</div>
+        <InputSearch />
+        {isMobile ? (
+          <div className={classes.hamburger}>
+            <MdMenu />
+          </div>
+        ) : (
+          <div className={classes.user}>User</div>
+        )}
+      </div>
+    </div>
   );
 };
