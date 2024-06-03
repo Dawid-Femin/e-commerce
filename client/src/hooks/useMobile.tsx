50d29386acb79ee1b450ec9a1ee44adb6ea13d6f
Dawid-Femin <d.femin@wp.pl>
2024-06-03 23:30:10 +0200

    Added useMobile hook

---
+++ client/src/hooks/useMobile.tsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+const useMobile = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 1024);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 1024);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return isSmallScreen;
+};
+
+export default useMobile;
