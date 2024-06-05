b192c815de77bc2f3996c62e63116350de3ccf9e
Dawid-Femin <d.femin@wp.pl>
2024-06-06 00:25:44 +0200

    Added HeroSlider to Home view

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,9 +1,13 @@
+import classes from "./home.module.scss";
+import { HeroSlider } from "../../components/HeroSlider/HeroSlider.tsx";
+import images from "../../assets/images.json";
+
 const Home = () => {
-    return (
-        <div>
-            Homw
-        </div>
-    );
+  return (
+    <div className={classes.root}>
+      <HeroSlider images={images} />
+    </div>
+  );
 };
 
 export default Home;
